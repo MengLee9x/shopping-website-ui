77a7dc31e686d346d2c1fc7f1cebf6cbcb782105
NhatMinh <@dstroke85@gmail.com>
2024-12-11 01:00:24 +0700

    update UI

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
     <StyledContainer>
       <Typography />
       <Router>
-
         <Routes>
           <Route path="/" element={<HomePage />} />
         </Routes>
--- src/components/SaleAdvertisement.js
+++ src/components/SaleAdvertisement.js
@@ -4,9 +4,25 @@ const StyledWrapper = styled.div`
     display: flex;
     background: conic-gradient(#E0C340, #DFC23E, #E1C441, #E3C743, #E4C542);
     height: 968px;
+    font-size: 40px;
 
     .image {
         width: 50%;
+    }
+    .notification {
+        width: 50%;
+        padding: 130px 200px 70px 155px;
+
+        .sale-banner-text {
+            display: flex;
+            flex-direction: column;
+            font-weight: 900;
+            font-size: 110px;
+            word-spacing: 100vw;
+            margin-bottom: 15px;
+            line-height: 140px;
+            z-index: 10;
+        }
     }
 `
 
@@ -16,7 +32,21 @@ const SaleAdvertisement = () => {
         <StyledWrapper>
             <div className="image">Hello</div>
             <div className="notification">
-                content
+                <div className="sale-banner-text">
+                    <div>PAYDAY</div>
+                    <div>
+                        SALE NOW
+                    </div>
+                </div>
+                <div className="notification-content">
+                    Spend minimal $100 get 30% off
+                    voucher code for your next purchase
+                    <span>1 June - 10 June 2021</span>
+                    *Terms & Conditions apply
+                </div>
+                <div className="">
+
+                </div>
             </div>
         </StyledWrapper>
     )
